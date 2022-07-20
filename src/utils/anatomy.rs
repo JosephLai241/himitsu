0f7a03c5222ff55d8252985ef8ca1322abed47b6
Joseph Lai <jlai24142@gmail.com>
2022-07-19 22:20:30 -0500

    Adding functions to manipulate the Anatomy struct

---
+++ src/utils/anatomy.rs
@@ -0,0 +1,48 @@
+//! Contains utilities for setting metadata for the stored secret. In other words, the anatomy of
+//! the secret.
+
+use chrono::Local;
+
+use crate::models::metadata::Anatomy;
+
+/// Create a new `Anatomy` for a particular secret.
+pub fn create_new_anatomy(category: &Option<String>, tags: &Option<Vec<String>>) -> Anatomy {
+    let category = match category {
+        Some(category_name) => category_name.to_string(),
+        None => "Unclassified".to_string(),
+    };
+    let tags = match tags {
+        Some(tag_values) => tag_values.clone(),
+        None => vec![],
+    };
+
+    Anatomy {
+        category,
+        date_created: Local::now().format("%m-%d-%Y %H:%M:%S").to_string(),
+        last_accessed: None,
+        tags,
+    }
+}
+
+/// Create a new `Anatomy` from an existing `Anatomy`.
+pub fn create_anatomy_from_existing(
+    category: &Option<String>,
+    existing_anatomy: &Anatomy,
+    tags: &Option<Vec<String>>,
+) -> Anatomy {
+    let category = match category {
+        Some(category_name) => category_name.to_string(),
+        None => "None".to_string(),
+    };
+    let tags = match tags {
+        Some(tag_values) => tag_values.clone(),
+        None => vec![],
+    };
+
+    Anatomy {
+        category,
+        date_created: existing_anatomy.date_created.clone(),
+        last_accessed: Some(Local::now().format("%m-%d-%Y %H:%M:%S").to_string()),
+        tags,
+    }
+}
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,5 +1,6 @@
 //! Contains utilities used throughout `skeleton`.
 
+pub mod anatomy;
 pub mod clipboard;
 pub mod config;
 pub mod paint;
