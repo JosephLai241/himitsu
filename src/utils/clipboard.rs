c0af289b642a8a8f07983d62825f6585daa73e1c
Joseph Lai <jlai24142@gmail.com>
2022-07-21 03:59:05 -0500

    Updating the function signature

--- src/utils/clipboard.rs
+++ src/utils/clipboard.rs
@@ -5,7 +5,7 @@ use copypasta::{ClipboardContext, ClipboardProvider};
 use crate::errors::SkeletonsError;
 
 /// Copy the decrypted secret to the system clipboard.
-pub fn set_clipboard(secret_value: &str) -> Result<(), SkeletonsError> {
+pub fn set_clipboard(secret_value: String) -> Result<(), SkeletonsError> {
     match ClipboardContext::new() {
         Ok(mut context) => context.set_contents(secret_value.to_owned()).map_or_else(
             |error| Err(SkeletonsError::ClipboardContentsError(error.to_string())),
