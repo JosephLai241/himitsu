c662cea065728b111c6620f7fc7ecabdc7c2faec
Joseph Lai <jlai24142@gmail.com>
2022-07-21 00:51:03 -0500

    Updating the configuration for prompts. Renaming the function to a more suitable name

--- src/prompts/config.rs
+++ src/prompts/config.rs
@@ -2,8 +2,8 @@
 
 use inquire::ui::{Attributes, Color, RenderConfig, StyleSheet, Styled};
 
-/// Get the configuration settings for the authentication prompts.
-pub fn get_authentication_config() -> RenderConfig {
+/// Get the configuration settings for the application.
+pub fn get_inquire_config() -> RenderConfig {
     let mut render_config = RenderConfig::default();
 
     render_config.answer = StyleSheet::new()
@@ -11,11 +11,16 @@ pub fn get_authentication_config() -> RenderConfig {
         .with_fg(Color::DarkGreen);
     render_config.error_message = render_config
         .error_message
-        .with_prefix(Styled::new("🤔").with_fg(Color::DarkRed));
+        .with_message(
+            StyleSheet::new()
+                .with_attr(Attributes::BOLD)
+                .with_fg(Color::DarkRed),
+        )
+        .with_prefix(Styled::new("🤔"));
     render_config.help_message = StyleSheet::new()
-        .with_attr(Attributes::ITALIC)
+        .with_attr(Attributes::BOLD)
         .with_fg(Color::DarkYellow);
-    render_config.prompt_prefix = Styled::new("☠️ ");
+    render_config.prompt_prefix = Styled::new("¿");
 
     render_config
 }
