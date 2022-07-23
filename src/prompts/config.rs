5cc6a85131a28510b47f33272a429b150dda4af9
Joseph Lai <jlai24142@gmail.com>
2022-07-23 04:19:38 -0500

    Updating the way attributes are set in the inquire RenderConfig based on the enum passed

--- src/prompts/config.rs
+++ src/prompts/config.rs
@@ -2,8 +2,16 @@
 
 use inquire::ui::{Attributes, Color, RenderConfig, StyleSheet, Styled};
 
+/// Variants for configuration types to use in the application.
+pub enum ConfigType {
+    /// Get the confirmation prompt configuration for the application.
+    Confirm,
+    /// Get the standard configuration for the application.
+    Standard,
+}
+
 /// Get the configuration settings for the application.
-pub fn get_inquire_config() -> RenderConfig {
+pub fn get_inquire_config(config_type: ConfigType) -> RenderConfig {
     let mut render_config = RenderConfig::default();
 
     render_config.answer = StyleSheet::new()
@@ -20,7 +28,17 @@ pub fn get_inquire_config() -> RenderConfig {
     render_config.help_message = StyleSheet::new()
         .with_attr(Attributes::BOLD)
         .with_fg(Color::DarkYellow);
-    render_config.prompt_prefix = Styled::new("¿");
+    render_config.prompt_prefix = Styled::new("㊙");
+
+    match config_type {
+        ConfigType::Confirm => {
+            render_config.prompt = render_config
+                .prompt
+                .with_attr(Attributes::BOLD)
+                .with_fg(Color::DarkYellow)
+        }
+        _ => {}
+    }
 
     render_config
 }
