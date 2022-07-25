a4629f84c8c37801e2f49fb0c9d743154d0350c8
Joseph Lai <jlai24142@gmail.com>
2022-07-25 03:07:21 -0500

    Adding a create_from method for the Anatomy struct. Deriving serde's Deserialize and Serialize for LookupMatch

--- src/models/metadata.rs
+++ src/models/metadata.rs
@@ -2,6 +2,7 @@
 
 use std::collections::HashMap;
 
+use chrono::Local;
 use serde::{Deserialize, Serialize};
 
 /// Contains metadata for each secret.
@@ -17,6 +18,19 @@ pub struct Anatomy {
     pub last_accessed: Option<String>,
     /// The tags associated with this secret.
     pub tags: Vec<String>,
+}
+
+impl Anatomy {
+    /// Create a new `Anatomy` for a secret from a defined `category`, `label`, and `tags`.
+    pub fn create_from(category: String, label: String, tags: Vec<String>) -> Self {
+        Self {
+            category,
+            date_created: Local::now().format("%m-%d-%Y %H:%M:%S").to_string(),
+            label,
+            last_accessed: None,
+            tags,
+        }
+    }
 }
 
 /// Contains the lookup table for secrets.
@@ -57,7 +71,7 @@ impl LookupTable {
 
 /// This struct temporarily holds a modified entry in the lookup table while searching for matches,
 /// which includes the secret's hash ID as well as its `Anatomy`.
-#[derive(Debug)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct LookupMatch {
     /// The secret's corresponding `Anatomy`.
     pub anatomy: Anatomy,
