3e7df1962dde68a955c36a48ed87ca57611a8942
Joseph Lai <jlai24142@gmail.com>
2022-08-11 00:01:50 -0500

    Updating execution flow for the application

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,6 @@ mod prompts;
 mod utils;
 
 use cli::{subcommands, Args};
-use errors::HimitsuError;
 use prompts::{authenticate, setup};
 use utils::{config, paint};
 
@@ -36,48 +35,33 @@ fn main() {
         match config::get_encryption_values() {
             Ok(crypt_json) => match crypt_json {
                 Some(encryption_values) => {
-                    if let Err(error) = authenticate::authenticate_user(&encryption_values) {
-                        paint::paint_error(error)
-                    } else {
-                        if let Some(subcommand) = &args.subcommand {
-                            if let Err(error) =
-                                subcommands::run_subcommands(&encryption_values, subcommand)
-                            {
-                                paint::paint_error(error);
+                    match authenticate::authenticate_user(&encryption_values) {
+                        Ok(password) => {
+                            if let Some(subcommand) = &args.subcommand {
+                                if let Err(error) =
+                                    subcommands::run_subcommands(&password, subcommand)
+                                {
+                                    paint::paint_error(error);
+                                }
+                            } else {
+                                println!(
+                                    "{}",
+                                    Color::Fixed(172)
+                                        .paint(String::from_utf8_lossy(&ASCII_ART[..]))
+                                );
                             }
-                        } else {
-                            // TODO: IMPLEMENT TUI FOR SECRETS.
-                            unimplemented!()
                         }
+                        Err(error) => paint::paint_error(error),
                     }
                 }
                 None => {
+                    println!(
+                        "{}",
+                        Color::Fixed(172).paint(String::from_utf8_lossy(&ASCII_ART[..]))
+                    );
+
                     if let Err(error) = setup::run_initial_setup_prompts() {
                         paint::paint_error(error);
-                    }
-
-                    match config::get_encryption_values() {
-                        Ok(crypt_json) => match crypt_json {
-                            Some(encryption_values) => {
-                                if let Some(subcommand) = &args.subcommand {
-                                    // TODO:
-                                    //      UPDATE THIS TO CHECK WHICH SUBCOMMAND IS CALLED. IF
-                                    //      CALLED ANYTHING OTHER THAN THE "ADD" SUBCOMMAND, PRINT A
-                                    //      WARNING SAYING YOU NEED TO ADD SHIT BEFORE YOU CAN USE IT
-                                    //      AND EXIT OUT.
-                                    if let Err(error) =
-                                        subcommands::run_subcommands(&encryption_values, subcommand)
-                                    {
-                                        paint::paint_error(error);
-                                    }
-                                } else {
-                                    // TODO: IMPLEMENT TUI FOR SECRETS.
-                                    unimplemented!()
-                                }
-                            }
-                            None => paint::paint_error(HimitsuError::ApplicationError),
-                        },
-                        Err(error) => paint::paint_error(error),
                     }
                 }
             },
